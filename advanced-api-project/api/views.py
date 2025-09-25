765dc180f3e0d6d3d14ebc2d517b0fd5fb92a271
William Eddie Plange <Wilplanh@gmail.com>
2025-09-25 18:32:52 +0000

    Advanced_api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 
