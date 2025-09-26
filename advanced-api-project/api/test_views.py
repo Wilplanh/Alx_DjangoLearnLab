f0c00bff485b3455b195aed84c99872006451c29
William Eddie Plange <Wilplanh@gmail.com>
2025-09-26 11:22:27 +0000

    Advanced_api

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,21 +1,36 @@
 from django.test import TestCase
 from api.models import Author, Book
 from rest_framework import status, response.data
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 
 # Create your tests here.
-class AuthorAPITestCase(TestCase):
-    def setUp(self):
+def setUp(self):
+        # Create test user
+        self.user = User.objects.create_user(username="tester", password="testpass123")
+
+        # Auth client
+        self.client = APIClient()
+        self.client.login(username="tester", password="testpass123")
+
+        # Create test author and book
         self.author = Author.objects.create(name="J.K. Rowling")
+        self.book = Book.objects.create(
+            title="Harry Potter",
+            author=self.author,
+            publication_year=1997
+        )
 
-    def test_author_creation(self):
-        self.assertEqual(self.author.name, "J.K. Rowling")
-        self.assertIsInstance(self.author, Author)
+   def test_author_creation(self):
+       response = self.client.post("/api/authors/", {"name": "J.K. Rowling"})
+       self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+       self.assertEqual(Author.objects.count(), 2)
 
-class BookAPITestCase(TestCase):
-    def setUp(self):
-        self.author = Author.objects.create(name="J.K. Rowling")
-        self.book = Book.objects.create(title="Harry Potter and the Philosopher's Stone", author=self.author)
-
-    def test_book_creation(self):
-        self.assertEqual(self.book.title, "Harry Potter and the Philosopher's Stone")
-        self.assertIsInstance(self.book, Book)
+   def test_book_creation(self):
+       response = self.client.post("/api/books/", {
+           "title": "Harry Potter",
+           "author": self.author.id,
+           "publication_year": 1997
+       })
+       self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+       self.assertEqual(Book.objects.count(), 2)
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .serializers import AuthorSerializer, BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import ReadOnly
 
 
 
