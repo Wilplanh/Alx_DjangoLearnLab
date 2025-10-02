58b85f10f4741ba7056563369b57a55f3cd0507c
William Eddie Plange <Wilplanh@gmail.com>
2025-10-02 23:51:04 +0000

    blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.models import login, logout, authenticate
+from django.http import HttpResponse
+from django.contrib.auth.forms import usercreationform
 
 # Create your views here.
 
 def home(request):
     return render(request, 'base.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'base.html')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'base.html')
+
+
+
+
