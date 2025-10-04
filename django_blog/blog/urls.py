85bb67fae9a872ccab40bfac77fd8b76726dabc6
William Eddie Plange <Wilplanh@gmail.com>
2025-10-04 00:15:39 +0000

    blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('create/', views.CreateView, name='create'),
+    path('update/<int:pk>/', views.UpdateView, name='update'),
+    path('delete/<int:pk>/', views.DeleteView, name='delete'),
 ]
