aa196d989192ac289d1c3edb1d8c1d27f96855b4
William Eddie Plange <Wilplanh@gmail.com>
2025-10-04 23:11:45 +0000

    blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post, Comment, tag
+from taggit.forms import TagWidget
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['author', 'content']
+        fields = ['author', 'content', 'tags']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+            'tags': TagWidget(),
+        }
 
 # add validation if needed
     def clean_content(self):
@@ -14,7 +20,4 @@ class CommentForm(forms.ModelForm):
             raise forms.ValidationError("Content is required.")
         return content
 
-class TagForm(forms.ModelForm):
-    class Meta:
-        model = tag
-        fields = ['name']
+
