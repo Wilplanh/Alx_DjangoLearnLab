b362c3dc4d7685a592de6ae807259afd51b7c033
William Eddie Plange <Wilplanh@gmail.com>
2025-10-04 22:10:52 +0000

    blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -78,3 +78,44 @@ class AuthorDeleteView(LoginRequiredMixin, UserPassesTestMixin):
 
     def get(self, request, *args, **kwargs):
         return HttpResponse("This is the author delete view")
+
+# Comment Views
+@login_required
+def CommentCreateView(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'add_comment.html', {'form': form, 'post': post})
+
+@login_required
+def CommentUpdateView(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return HttpResponse("You are not authorized to edit this comment.")
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=comment.post.pk)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'edit_comment.html', {'form': form, 'comment': comment})
+
+@login_required
+def CommentDeleteView(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return HttpResponse("You are not authorized to delete this comment.")
+    if request.method == 'POST':
+        post_pk = comment.post.pk
+        comment.delete()
+        return redirect('post_detail', pk=post_pk)
+    return render(request, 'delete_comment.html', {'comment': comment})
