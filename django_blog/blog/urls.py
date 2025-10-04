eb0e478945bef656a9ac75555608a4e1b3dd19fb
William Eddie Plange <Wilplanh@gmail.com>
2025-10-04 23:37:22 +0000

    blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('post/comment/<int:pk>/update/', views.CommentUpdateView, name='edit_comment'),
     path('post/comment/<int:pk>/delete/', views.CommentDeleteView, name='delete_comment'),
     path('search/', views.search_posts, name='search_posts'),
-    path('tags/<slug:slug>/', views.posts_by_tag, name='posts_by_tag'),
+    path('tags/<slug:tag_slug>/', views.PostsByTagList, name='posts_by_tag'),
 ]
