bbff63697f1e5d614c12db0e5e814e4b2e2ed4a2
William Eddie Plange <Wilplanh@gmail.com>
2025-10-09 15:14:39 +0000

    Social Media

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +14,12 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+
+class LoginViewSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+class profileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.contrib.auth.models import AbstractUser
 from django.views import View
 from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
 # Create your views here.
 
-class register(View):
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(View):
     def get(self, request):
-        return HttpResponse("Register Page")
+        return HttpResponse("Login Page")
+
+class ProfileView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = profileSerializer
