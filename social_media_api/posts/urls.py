5e5502ac3ce76fa4460f3eeb3c5132f7c5c2364c
William Eddie Plange <Wilplanh@gmail.com>
2025-10-10 14:32:46 +0000

    Social Media

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,8 +5,9 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-router.register(r'feed', FeedView)
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', FeedView.as_view(), name='feed')
 ]
