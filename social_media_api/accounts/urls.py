9081cb940984719c95ccd39eec2e3d2c98e81316
William Eddie Plange <Wilplanh@gmail.com>
2025-10-10 14:22:00 +0000

    Social Media

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name=follow),
 ]
