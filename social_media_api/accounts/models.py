fe2f6e8920857f2eb8206dcf2a8365f8d0f8090e
William Eddie Plange <Wilplanh@gmail.com>
2025-10-10 13:47:29 +0000

    Social Media

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from rest_framework import permissions, serializers, generics
+from 
 
 # Create your models here.
 
@@ -7,6 +9,7 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    
 
     # Fix the group and permission fields
     groups = models.ManyToManyField(
@@ -28,3 +31,13 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class follow_user(models.Model):
+    permissions = [permissions.IsAuthenticated]
+    user = models.ForeignKey(CustomUser, related_name='follower', on_delete=models.CASCADE
+
+
+class unfollow_user(models.Model):
+    permissions = [permissions.IsAuthenticated]
+    user = models.ForeignKey(CustomUser, related_name='unfollower', on_delete=models.CASCADE)
+    unfollowed_user = models.ForeignKey(CustomUser, related_name='unfollowed', on_delete=models.CASCADE)
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework,pigmentation import PageNumberPagination
 
 # Create your views here.
+
+# Pagination class
+class PostPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = PostPagination
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    search_fields = ['title', 'content']
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    pagination_class = PostPagination
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
